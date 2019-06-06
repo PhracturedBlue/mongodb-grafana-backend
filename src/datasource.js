9972897a1b0d8e2c908b3243046046990c0d6fe1
PhracturedBlue <rc2012@pblue.org>
2019-06-06 11:28:24 -0700

    Convert to backend plugin to enable alerting

--- src/datasource.js
+++ src/datasource.js
@@ -2,14 +2,16 @@ import _ from "lodash";
 
 export class GenericDatasource {
 
-  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+  constructor(instanceSettings, $q, backendSrv, templateSrv, timeSrv) {
     this.type = instanceSettings.type;
     this.url = instanceSettings.url;
     this.name = instanceSettings.name;
     this.db = { 'url' : instanceSettings.jsonData.mongodb_url, 'db' : instanceSettings.jsonData.mongodb_db }
+    this.id = instanceSettings.id;
     this.q = $q;
     this.backendSrv = backendSrv;
     this.templateSrv = templateSrv;
+    this.timeSrv = timeSrv;
     this.withCredentials = instanceSettings.withCredentials;
     this.headers = {'Content-Type': 'application/json'};
     if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
@@ -26,15 +28,49 @@ export class GenericDatasource {
       return this.q.when({data: []});
     }
 
-    return this.doRequest({
-      url: this.url + '/query',
-      data: query,
-      method: 'POST'
+    return this.doRequest(query)
+    .then(result => {
+      var res= [];
+      _.forEach(result.data.results, r => {
+        _.forEach(r.series, s => {
+          res.push({target: s.name, datapoints: s.points});
+        })
+        _.forEach(r.tables, t => {
+          t.type = 'table';
+          t.refId = r.refId;
+          res.push(t);
+        })
+      })
+
+      result.data = res;
+      return result;
     });
   }
 
+  buildQueryParameters(options) {
+    //remove placeholder targets
+    options.targets = _.filter(options.targets, target => {
+      return target.target !== 'select metric';
+    });
+
+    var targets = _.map(options.targets, target => {
+      return {
+        queryType: 'query',
+        target: this.templateSrv.replace(target.target, options.scopedVars, ''),
+        refId: target.refId,
+        hide: target.hide,
+        type: target.type || 'timeserie',
+        datasourceId: this.id
+      };
+    });
+
+    options.targets = targets;
+
+    return options;
+  }
+
   testDatasource() {
-    return this.doRequest({
+    return this.doDirectRequest({
       url: this.url + '/',
       data : { db : this.db },
       method: 'POST',
@@ -59,65 +95,97 @@ export class GenericDatasource {
       rangeRaw: options.rangeRaw
     };
 
-    return this.doRequest({
+    if (this.templateSrv.getAdhocFilters) {
+      query.adhocFilters = this.templateSrv.getAdhocFilters(this.name);
+    } else {
+      query.adhocFilters = [];
+    }
+
+    return this.doDirectRequest({
       url: this.url + '/annotations',
       method: 'POST',
       data: annotationQuery
     }).then(result => {
       response.data.$$status = result.status;
       response.data.$$config = result.config;
-    return result.data;
+      return result.data;
     });
   }
 
   metricFindQuery(query) {
-    var interpolated = {
-        target: this.templateSrv.replace(query, null, '')
-        };
-    interpolated.db = this.db
-
-    return this.doRequest({
-      url: this.url + '/search',
-      data: interpolated,
-      method: 'POST',
-    }).then(this.mapToTextValue);
+    var range = this.timeSrv.timeRange();
+    var targets = [{
+      queryType: 'search',
+      target: this.templateSrv.replace(query, null, ''),
+      db: this.db,
+      datasourceId: this.id,
+      refId: "search",
+    }];
+    var options = {
+      range: range,
+      targets: targets
+    };
+    return this.doRequest(options).then(this.mapToTextValue);
   }
 
   mapToTextValue(result) {
-    return _.map(result.data, (d, i) => {
-      if (d && d.text && d.value) {
-        return { text: d.text, value: d.value };
-      } else if (_.isObject(d)) {
-        return { text: d, value: i};
+    var table = result.data.results.search.tables[0];
+
+    if (!table) {
+      return [];
+    }
+
+    return _.map(table.rows, (row, i) => {
+      if (row.length > 1) {
+        return { text: row[0], value: row[1] };
+      } else if (_.isObject(row[0])) {
+        return { text: row[0], value: i};
       }
-      return { text: d, value: d };
+      return { text: row[0], value: row[0] };
     });
   }
 
-  doRequest(options) {
+  doDirectRequest(options) {
     options.withCredentials = this.withCredentials;
     options.headers = this.headers;
 
     return this.backendSrv.datasourceRequest(options);
   }
 
-  buildQueryParameters(options) {
-    //remove place holder targets
-    options.targets = _.filter(options.targets, target => {
-      return target.target !== 'select metric';
+  doRequest(options) {
+    return this.backendSrv.datasourceRequest({
+      url: '/api/tsdb/query',
+      method: 'POST',
+      data: {
+        from: options.range.from.valueOf().toString(),
+        to: options.range.to.valueOf().toString(),
+        queries: options.targets,
+      }
     });
-
-    var targets = _.map(options.targets, target => {
-      return {
-        target: this.templateSrv.replace(target.target, options.scopedVars, ''),
-        refId: target.refId,
-        hide: target.hide,
-        type: target.type || 'timeserie'      
-      };
-    });
-
-    options.targets = targets;
-
-    return options;
   }
+
+  getTagKeys(options) {
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-keys',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
+  getTagValues(options) {
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-values',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
 }
